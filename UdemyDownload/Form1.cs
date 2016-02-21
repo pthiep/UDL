de1c601863ec037f7bd7ba73bb771e96825f695e
pthiep <phantanhiepnt@gmail.com>
2016-02-21 22:09:16 +0700

    UDL
    UDL

---
+++ UdemyDownload/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;    
+
+namespace UdemyDownload
+{
+    public partial class Form1 : Form
+    {
+        bool t = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            Session ss = new Session("phantanhiepnt@gmail.com", "01694424958");
+            ss.Login();
+            textBox1.Text = ss.test;
+            richTextBox1.Text = ss.get_csrf_token();
+        }
+    }
+}
---
+++ UdemyDownload/Session.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using HtmlAgilityPack;
+
+namespace UdemyDownload
+{
+    public class Session
+    {
+        public string test;
+
+        private string User;
+        private string Pass;
+
+        private string LOGIN_URL = "https://www.udemy.com/join/login-popup/?displayType=ajax&display_type=popup&showSkipButton=1&returnUrlAfterLogin=https%3A%2F%2Fwww.udemy.com%2F&next=https%3A%2F%2Fwww.udemy.com%2F&locale=en_US";
+        private string LOGIN_GET = "https://www.udemy.com/join/login-popup";
+
+        private string useragent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36";
+
+        CookieContainer cookieContainer = new CookieContainer();
+
+        public Session(string user, string pass)
+        {
+            User = user;
+            Pass = pass;
+        }
+
+        public string get_csrf_token()
+        {
+            var request = (HttpWebRequest)WebRequest.Create(LOGIN_GET);
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(responseString);
+            return htmlDoc.DocumentNode.SelectSingleNode("//input[@type='hidden' and @name='csrfmiddlewaretoken']").Attributes["value"]
+                    .Value;
+        }
+
+        public void Login()
+        {
+            string csrf_token = get_csrf_token();
+            string postData = "isSubmitted=1&email=" + User + "&password=" + Pass + "&dislayType=ajax&csrfmiddlewaretoken=" + csrf_token;
+            
+            var request = (HttpWebRequest)WebRequest.Create(LOGIN_URL);
+            
+            var data = Encoding.ASCII.GetBytes(postData);
+
+            test = postData.ToString();
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.UserAgent = useragent;
+            request.Headers.Add("X-Requested-With", "XMLHttpRequest");
+            request.Referer = "https://www.udemy.com/join/login-popup";
+            request.CookieContainer = cookieContainer;
+            request.AllowAutoRedirect = false;
+            request.UseDefaultCredentials = true;
+            request.ContentLength = data.Length;
+
+            request.CookieContainer = new CookieContainer();
+
+            using (Stream stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+
+            //request.CookieContainer.GetCookies("access_token");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                                    
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+        }
+    }
+}
